9e47abd2f3ec16f4b3b33beeb51b58633d249512
Jeremy <jerekith2@gmail.com>
2015-10-29 16:31:36 +0300

    Remove redudant code and prettify code

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,27 +1,27 @@
-describe("Inverted Index tests", function() {
+describe('Inverted Index tests', function() {
   var invertedIndex = new Index();
-  var results = [];
 
   // Before each test can run do this.
   beforeEach(function(done) {
-  invertedIndex.createIndex("./books.json").done(function(data) {
-    results = data;
-    invertedIndex.getIndex(data);
-    done();
+    invertedIndex.createIndex('./books.json').done(function(data) {
+      invertedIndex.results = data;
+      invertedIndex.getIndex(data);
+      done();
     });
   });
 
-  describe("Read book data", function() {
+  describe('Read book data', function() {
     // Check whether the JSON data is an array containing objects
-    it("Loads JSON data successfully", function() {
-      expect(results).not.toBeUndefined();
-      expect(results.length).not.toEqual(0);
-      expect(results.length).toBe(2);
-      expect(Array.isArray(results)).toBe(true);
+    it('Loads JSON data successfully', function() {
+      expect(invertedIndex.results).not.toBeUndefined();
+      expect(invertedIndex.results.length).not.toEqual(0);
+      expect(invertedIndex.results.length).toBe(2);
+      expect(Array.isArray(invertedIndex.results)).toBe(true);
     });
-    it("Contents of the array are objects", function() {
-      expect(results[0] instanceof Object).toBe(true);
-      expect(results[1] instanceof Object).toBe(true);
+    // The array should contain objects.
+    it('Contents of the array are objects', function() {
+      expect(invertedIndex.results[0] instanceof Object).toBe(true);
+      expect(invertedIndex.results[1] instanceof Object).toBe(true);
     });
   });
 
@@ -34,10 +34,11 @@ describe("Inverted Index tests", function() {
       expect(index.fellowship).toBeDefined();
     });
 
-    it('ensure index is correct', function() {;
+    it('ensure index is correct', function() {
       var index = invertedIndex.dataIndex;
-        indexKeys = Object.keys(index);
+      var indexKeys = Object.keys(index);
 
+      // Index contains words from the file.
       expect(indexKeys).toContain('powerful');
       expect(indexKeys).not.toContain('a');
       expect(indexKeys).toContain('wonderland');
@@ -53,27 +54,27 @@ describe("Inverted Index tests", function() {
 
   describe('Search index', function() {
 
-  it('returns the correct results when searched', function() {
-    expect(invertedIndex.searchIndex('jeremy')).toEqual([-1]);
-    expect(Array.isArray(invertedIndex.searchIndex('alice'))).toBe(true);
-    expect(invertedIndex.searchIndex('alice')).toContain(0);
-    expect(invertedIndex.searchIndex('fellowship')).toContain(1);
-  });
+    it('returns the correct results when searched', function() {
+      expect(invertedIndex.searchIndex('jeremy')).toEqual([-1]);
+      expect(Array.isArray(invertedIndex.searchIndex('alice'))).toBe(true);
+      expect(invertedIndex.searchIndex('alice')).toContain(0);
+      expect(invertedIndex.searchIndex('fellowship')).toContain(1);
+    });
 
-  it('can search more than one term', function() {
-    // All words that don't exist should result in -1.
-    expect(invertedIndex.searchIndex('jeremy', 'superhero')).not.toContain(0);
-    expect(invertedIndex.searchIndex('jeremy', 'superhero', 'javascript')).not.toContain(1);
+    it('can search more than one term', function() {
+      // All words that don't exist should result in -1.
+      expect(invertedIndex.searchIndex('jeremy', 'superhero')).not.toContain(0);
+      expect(invertedIndex.searchIndex('jeremy', 'superhero', 'javascript')).not.toContain(1);
 
-    // Result should be 1 or 0 for words that exist.
-    expect(invertedIndex.searchIndex('imagination', 'rings')).toContain(0);
-    expect(invertedIndex.searchIndex('alliance', 'wizard', 'dwarf')).toContain(1);
-  });
+      // Result should be 1 or 0 for words that exist.
+      expect(invertedIndex.searchIndex('imagination', 'rings')).toContain(0);
+      expect(invertedIndex.searchIndex('alliance', 'wizard', 'dwarf')).toContain(1);
+    });
 
-  it('can handle an array of search terms', function() {
-    // Give it an array of strings as an argument.
-    expect(invertedIndex.searchIndex(['rabbit', 'world'])).toContain(0);
-    expect(invertedIndex.searchIndex(['fellowship', 'destroy', 'powerful'])).toContain(1);
+    it('can handle an array of search terms', function() {
+      // Give it an array of strings as an argument.
+      expect(invertedIndex.searchIndex(['rabbit', 'world'])).toContain(0);
+      expect(invertedIndex.searchIndex(['fellowship', 'destroy', 'powerful'])).toContain(1);
+    });
   });
-});
 });
--- jasmine/src/inverted-index.js
+++ jasmine/src/inverted-index.js
@@ -1,49 +1,51 @@
 function Index() {
-  this.dataIndex;
-  this.createIndex = function (filepath) {
-  return $.getJSON(filepath);
-  }
+
+  "use strict";
+
+  this.createIndex = function(filepath) {
+    return $.getJSON(filepath)
+  };
+
   this.getIndex = function(bookArray) {
+
     var that = this;
+
     // List of common words to be excluded from the index.
-    var stopList = ["a", "an", "and", "as", "at", "but", "by", "each", "every", "for", 
-    "from", "her", "his", "in", "into", "its", "like", "my", "no", "nor",
-    "of", "off", "on", "onto", "or", "our", "out", "outside", "over", "past", 
-    "since", "so", "some", "than", "that", "the", "their", "this", "to", "up", "with"];
+    var stopList = ["a", "an", "and", "as", "at", "but", "by", "each", "every", "for",
+      "from", "her", "his", "in", "into", "its", "like", "my", "no", "nor",
+      "of", "off", "on", "onto", "or", "our", "out", "outside", "over", "past",
+      "since", "so", "some", "than", "that", "the", "their", "this", "to", "up", "with"
+    ];
+
     var arr = bookArray;
-    // initialise array to contain title and text strings from the JSON file.
-    var invIndex =[];
+
+    // initialise array to contain the words from the JSON file.
+    var words = [];
 
     // Loop through the array and add the title and text strings to the array words.
-    arr.forEach(function (book) {
-      (Object.keys(book)).forEach(function (keys) { 
-        var words = [];
-        (keys.split(/[.,:;" "\/ -]/)).forEach(function (word) {
-          if (word != "") {
+    arr.forEach(function(book) {
+      (Object.keys(book)).forEach(function(keys) {
+        (book[keys].split(/[.,:;" "\/ -]/)).forEach(function(word) {
+          if (word !== "") {
             // Make the word lowercase before adding it to the array.
             words.push(word.toLowerCase());
           }
         });
-        (book[keys].split(/[.,:;" "\/ -]/)).forEach(function (word) {
-          if (word != "") {
-            // Make the word lowercase before adding it to the array.
-            words.push(word.toLowerCase());
-          }
-        });
-        words.forEach(function (entries) {
-          invIndex.push(entries);
-        })
       });
     });
+
     // Sort the words alphabetically and remove duplicates.
-    invIndex = invIndex.sort().reduce(function(a, b){ if (b != a[0]) a.push(b); return a }, []);
-    
+    words = words.sort().reduce(function(a, b) {
+      if (b != a[0]) a.push(b);
+      return a;
+    }, []);
+
     // Initiliase array to contain the words to be indexed.
     var indexable = [];
 
     // Loop through singleWords and add words not in the stoplist to the final list of indexable words.
-    invIndex.forEach(function(singleword) {
-      if(stopList.indexOf(singleword) === -1) {
+    words.forEach(function(singleword) {
+      if (stopList.indexOf(singleword) === -1) {
         indexable.push(singleword);
       }
     });
@@ -55,17 +57,21 @@ function Index() {
     indexable.forEach(function(element) {
       arr.forEach(function(object, index) {
         (Object.keys(object)).forEach(function(key) {
-          if(((arr[index][key]).toLowerCase()).indexOf(element) != -1) {
-              wordIndex[element] = index;
+          if (((arr[index][key]).toLowerCase()).indexOf(element) != -1) {
+            wordIndex[element] = index;
           }
         });
       });
     });
     that.dataIndex = wordIndex;
-  }
+  };
+
   this.searchIndex = function(terms) {
     var that = this;
+
+    // Initialise array to contain the results of the search.
     var searchRes = [];
+
     if (Array.isArray(terms)) {
       terms.forEach(function(term) {
         if (term in that.dataIndex === true) {
@@ -73,9 +79,10 @@ function Index() {
         } else {
           searchRes.push(-1);
         }
-      })
+      });
     } else {
-      for(var i = 0; i < arguments.length; i++) {
+      // If the arguments provided are not arrays.
+      for (var i = 0; i < arguments.length; i++) {
         if (arguments[i] in that.dataIndex === true) {
           searchRes.push(that.dataIndex[arguments[i]]);
         } else {
@@ -84,5 +91,5 @@ function Index() {
       }
     }
     return searchRes;
-  }
+  };
 }
